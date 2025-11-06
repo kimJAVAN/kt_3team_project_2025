bab86d375a27543fc23e600cd67d783597e6140e
dpwls4052 <dpwls4052@naver.com>
2025-11-06 16:22:07 +0900

    test

--- src/features/Pay/WidgetCheckout.jsx
+++ src/features/Pay/WidgetCheckout.jsx
@@ -92,8 +92,7 @@ export function WidgetCheckoutPage({
         orderId: generateRandomString(),
         orderName: orderName,
         successUrl: `${window.location.origin}/kt_3team_project_2025/pay/success`,
-
-        failUrl: `${window.location.origin}/kt_3team_project_2025/pay/fail`,
+        failUrl: `${baseUrl}/pay/fail`,
         customerEmail: 'customer123@gmail.com',
         customerName: '김토스',
       });
